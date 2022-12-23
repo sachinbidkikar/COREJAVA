3fb6eeee0e90b643c65e51693e4f0faaedfe6b23
Sachin Bidikikar <35056880+sachinbidkikar@users.noreply.github.com>
2022-12-23 22:46:31 +0530

    Evenodd

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number ");
+        int number = sc.nextInt();
+        if(number % 2 == 0){
+            System.out.println("given number is an even number");
+        }
+        else {
+            System.out.println("given number is an odd number");
+        }
+    }
+}
